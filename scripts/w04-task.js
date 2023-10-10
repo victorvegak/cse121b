5673a37af7eafb335fa64bf22c65a197f616b09b
victorvegak <vvega28@outlook.com>
2023-10-10 01:34:27 -0600

    W04-Task completed

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -1,28 +1,73 @@
 /* LESSON 3 - Programming Tasks */
 
 /* Profile Object  */
-
-
-
-
-/* Populate Profile Object with placesLive objects */
-
-
-
+let myProfile = {
+    name : "Victor Vega", 
+    photo : "images/profileimg.png",
+    favoriteFoods:[
+        'Rice',
+        'pizza',
+        'Hamburguer',
+        'Pasta',
+        'IceCream',
+    ],
+    hobbies : [
+        'Gaming',
+        'Watch movies',
+        'Listen Music',
+        'Read'
+    ],
+    placesLived: [{ /* Populate Profile Object with placesLive objects */
+        place: "Manangua, Nicaragua.",
+        length: "27 years",
+    },
+    {
+        place: "Esterillo, Costa Rica",
+        length: "1 year",   
+    },
+    ],
+};
 
 /* DOM Manipulation - Output */
 
 /* Name */
-
+document.querySelector('#name').textContent = myProfile.name;
 /* Photo with attributes */
-
+const photoElement = document.querySelector("#photo");
+photoElement.src = myProfile.photo;
+photoElement.alt = myProfile.name;
 
 /* Favorite Foods List*/
-
-
+const favoriteFoodsList = document.querySelector("#favorite-foods");
+myProfile.favoriteFoods.forEach(food => {
+    const listItem = document.createElement('li');
+    listItem.textContent = food;
+    favoriteFoodsList.appendChild(listItem);
+},
+);
 /* Hobbies List */
-
-
+const hobbiesList = document.querySelector("#hobbies");
+myProfile.hobbies.forEach(hobby => {
+    const listItem = document.createElement('li');
+    listItem.textContent = hobby;
+    hobbiesList.appendChild(listItem);
+},
+);
 /* Places Lived DataList */
+const placesLivedlist = document.querySelector("#places-lived");
+
+myProfile.placesLived.forEach(place => {
+    let dt = document.createElement("dt");
+    dt.innerHTML = `<strong>${place.place}</strong>`;
+
+    let dd = document.createElement("dd");
+    dd.textContent = place.length;
+
+    placesLivedlist.appendChild(dt);
+    placesLivedlist.appendChild(dd);
+},
+);
+
+
 
 
