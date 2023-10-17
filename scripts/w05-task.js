0dc573480f4f4e2d4b6f14c438414dbaf1b500cf
victorvegak <vvega28@outlook.com>
2023-10-17 02:20:12 -0600

    updated

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -8,7 +8,7 @@ const templeList = [];
 const displayTemples = (temples) => {
     templesElement.innerHTML = "";
 
-    temples.forEach((temple) =>{
+    temples.forEach((temple) => {
         const article = document.createElement("article");
         const h3 = document.createElement("h3");
         h3.textContent = temple.templeName;
@@ -22,27 +22,27 @@ const displayTemples = (temples) => {
 };
 
 /* async get temples function using fetch()*/
-const getTemples = async ()=> {
+const getTemples = async () => {
     try {
         const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
-        if (response.ok){
+        if (response.ok) {
             templeList.length = 0;
             const data = await response.json();
             templeList.push(...data);
             displayTemples(templeList);
-        }else {
+        } else {
             console.error("Failed to fetch temple data.");
         }
-    }catch (error) {
+    } catch (error) {
         console.error("An error occurred while fetching temple data.", error);
     }
 };
-/* reset Function */ 
-const reset = () =>{
+/* reset Function */
+const reset = () => {
     templesElement.innerHTML = "";
 };
 
-/* sortBy Function*/ 
+/* sortBy Function*/
 const sortBy = (temples) => {
     const filter = document.getElementById("sortBy").value;
 
@@ -51,10 +51,10 @@ const sortBy = (temples) => {
             displayTemples(temples.filter((temple) => temple.location.includes("Utah")));
             break;
         case "notutah":
-            displayTemples(temples.filter((temple)=> !temple.location.includes("Utah")));
+            displayTemples(temples.filter((temple) => !temple.location.includes("Utah")));
             break;
         case "older":
-            displayTemples(temples.filter((temple)=> new Date (temple.dedicated)< new Date(1950, 0,1)));
+            displayTemples(temples.filter((temple) => new Date(temple.dedicated) < new Date(1950, 0, 1)));
             break;
         case "all":
             displayTemples(temples);
@@ -65,7 +65,6 @@ const sortBy = (temples) => {
 getTemples();
 
 /* event Listener */
-document.querySelector("#sortBy").addEventListener("change",() =>{
+document.querySelector("#sortBy").addEventListener("change", () => {
     sortBy(templeList);
 });
-
